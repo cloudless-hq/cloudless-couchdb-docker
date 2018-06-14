7304bcfd32aa383d67fc836fe01208ae9b87ce9e
till <till@php.net>
2018-06-14 11:18:37 +0200

    Refactor: move all start scripts into entrypoint
     - this allows us to have one script (to rule them all)
     - also allows us to get into a container for debugging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,62 @@
 set -e
 
 if [ "$1" = 'clouseau' ]; then
+    # setup configuration
+    echo '[clouseau]' > clouseau.ini
+    echo "name=${NODE_NAME}" >> clouseau.ini
+    echo "cookie=${ERLANG_COOKIE}" >> clouseau.ini
+    echo "dir=${INDEX_DIR}" >> clouseau.ini
 
-    #explicetly start local epmd since for scalang/clouseau there's no automatic launch mechanism like there is for erlang
+    # explicitely start local epmd since for scalang/clouseau there's no automatic launch mechanism like there is for erlang
     epmd -daemon
 
-    #who knows why, but test-classes are currently needed in order for clouseau to run
-    #export CP=$(ls -t /jar/clouseau-*/*.jar | tr '\n' ':'):/jar/test-classes
-
-    mvn scala:run \
-      -Dclouseau.name=${NODE_NAME}@${BIND_ADDRESS} \
-      -Dclouseau.cookie=${ERLANG_COOKIE} \
-      -Dclouseau.dir=${INDEX_DIR}
-    #exec scala -classpath "$CP" -Dclouseau.name=$ERL_NAME -Dclouseau.cookie=$ERL_COOKIE -Dclouseau.dir=$INDEX_DIR com.cloudant.clouseau.Main
+    mvn scala:run
 fi
 
+if [ "$1" = 'couchdb' ]; then
+  chown -R couchdb:couchdb $COUCHDB_PATH
+
+  chmod -R 0770 $COUCHDB_PATH/data
+
+  chmod 664 $COUCHDB_PATH/etc/*.ini
+  chmod 664 $COUCHDB_PATH/etc/local.d/*.ini
+  chmod 775 $COUCHDB_PATH/etc/*.d
+
+  if [ ! -z "$NODE_NAME" ] && ! grep "couchdb@" $COUCHDB_PATH/etc/vm.args; then
+    echo "-name $NODE_NAME" >> $COUCHDB_PATH/etc/vm.args
+  fi
+
+  if [ "$COUCHDB_USER" ] && [ "$COUCHDB_PASSWORD" ]; then
+    # Create admin
+    printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > $COUCHDB_PATH/etc/local.d/docker.ini
+    chown couchdb:couchdb $COUCHDB_PATH/etc/local.d/docker.ini
+  fi
+
+  if [ "$COUCHDB_SECRET" ]; then
+    # Set secret
+    printf "[couch_httpd_auth]\nsecret = %s\n" "$COUCHDB_SECRET" >> $COUCHDB_PATH/etc/local.d/docker.ini
+    chown couchdb:couchdb $COUCHDB_PATH/etc/local.d/docker.ini
+  fi
+
+  # if we don't find an [admins] section followed by a non-comment, display a warning
+  if ! grep -Pzoqr '\[admins\]\n[^;]\w+' $COUCHDB_PATH/etc/local.d/*.ini; then
+    # The - option suppresses leading tabs but *not* spaces. :)
+    cat >&2 <<-"EOWARN"
+****************************************************
+WARNING: CouchDB is running in Admin Party mode.
+         This will allow anyone with access to the
+         CouchDB port to access your database. In
+         Docker's default configuration, this is
+         effectively any other container on the same
+         system.
+         Use "-e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password"
+         to set it in "docker run".
+****************************************************
+EOWARN
+  fi
+
+  bin/couchdb
+fi
+
+# execute anything :-)
 exec "$@"

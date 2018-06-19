6f265dc757b95b043b5828a79529c0c6d9996e9a
till <till@php.net>
2018-06-19 18:15:58 +0200

    Update: publish to Docker hub
     - created script to build, tag and publish images
     - small local test setup to confirm our pushes work

---
+++ .travis/publish.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+declare -a files=("clouseau/Dockerfile" "couchdb/Dockerfile")
+
+base_path="$(pwd)"
+hub_org="cloudlesshq"
+
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+
+for docker_file in "${files[@]}"
+do
+  case "$docker_file" in
+    clouseau/Dockerfile)
+      docker_image="${hub_org}/test-clouseau"
+      docker_version="1.0-SNAPSHOT-g$(git rev-parse --short HEAD)"
+      build_path="${base_path}/clouseau"
+      ;;
+    couchdb/Dockerfile)
+      docker_image="${hub_org}/test-couchdb"
+      docker_version="2.1.1-350f591-g$(git rev-parse --short HEAD)"
+      build_path="${base_path}/couchdb"
+      ;;
+    *)
+      echo "Unknown file: ${docker_file}"
+      exit 1
+  esac
+
+  echo ""
+  echo "Pushing ${docker_image}:${docker_version} (and :latest)"
+  echo "Path: ${build_path}"
+
+  docker build -t ${docker_image}:latest -t ${docker_image}:${docker_version} -f "${build_path}/Dockerfile" . \
+    && docker push $docker_image:$docker_version \
+    && docker push $docker_image:latest
+done

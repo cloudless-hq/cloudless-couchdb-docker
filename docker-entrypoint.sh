b8a0d129c2436c6682c954525b047ad0717487e8
till <till@php.net>
2018-06-25 16:48:34 +0200

    Refactor: use variables to re-use paths (less error-prone)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,34 +24,38 @@ if [ "$1" = 'couchdb' ]; then
 
   chmod -R 0770 $COUCHDB_PATH/data
 
-  chmod 664 $COUCHDB_PATH/etc/*.ini
-  chmod 664 $COUCHDB_PATH/etc/default.d/*.ini
-  chmod 644 $COUCHDB_PATH/etc/local.d
-  chmod 775 $COUCHDB_PATH/etc/*.d
+  etc_dir="${COUCHDB_PATH}/etc"
+  default_dir="${etc_dir}/default.d"
+  local_dir="${etc_dir}/local.d"
+
+  chmod 664 $etc_dir/*.ini
+  chmod 664 $default_dir/*.ini
+  chmod 644 $etc_dir/local.d
+  chmod 775 $etc_dir/*.d
 
   echo "Setting up vm.args"
-  cat $COUCHDB_PATH/etc/vm.args-dist > $COUCHDB_PATH/etc/vm.args
-  echo "-setcookie '${ERLANG_COOKIE}'" >> $COUCHDB_PATH/etc/vm.args
-  echo "-name ${NODENAME}" >> $COUCHDB_PATH/etc/vm.args
+  cat $etc_dir/vm.args-dist > $etc_dir/vm.args
+  echo "-setcookie '${ERLANG_COOKIE}'" >> $etc_dir/vm.args
+  echo "-name ${NODENAME}" >> $etc_dir/vm.args
 
-  echo "Setting up dreyfus/clouseau"
-  echo "[dreyfus]" > $COUCHDB_PATH/etc/local.d/00-dreyfus.ini
-  echo "name = ${CLOUSEAU_NAME}" >> $COUCHDB_PATH/etc/local.d/00-dreyfus.ini
+  echo "Setting up dreyfus/clouseau: ${CLOUSEAU_NAME}"
+  echo "[dreyfus]" > $local_dir/00-dreyfus.ini
+  echo "name = ${CLOUSEAU_NAME}" >> $local_dir/00-dreyfus.ini
 
   if [ "$COUCHDB_USER" ] && [ "$COUCHDB_PASSWORD" ]; then
     # Create admin
-    printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > $COUCHDB_PATH/etc/local.d/02-docker.ini
-    chown couchdb:couchdb $COUCHDB_PATH/etc/local.d/02-docker.ini
+    printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > $local_dir/02-docker.ini
+    chown couchdb:couchdb $local_dir/02-docker.ini
   fi
 
   if [ "$COUCHDB_SECRET" ]; then
     # Set secret
-    printf "[couch_httpd_auth]\nsecret = %s\n" "$COUCHDB_SECRET" >> $COUCHDB_PATH/etc/local.d/02-docker.ini
-    chown couchdb:couchdb $COUCHDB_PATH/etc/local.d/02-docker.ini
+    printf "[couch_httpd_auth]\nsecret = %s\n" "$COUCHDB_SECRET" >> $local_dir/02-docker.ini
+    chown couchdb:couchdb $local_dir/02-docker.ini
   fi
 
   # if we don't find an [admins] section followed by a non-comment, display a warning
-  if ! grep -Pzoqr '\[admins\]\n[^;]\w+' $COUCHDB_PATH/etc/local.d/*.ini; then
+  if ! grep -Pzoqr '\[admins\]\n[^;]\w+' $local_dir/*.ini; then
     # The - option suppresses leading tabs but *not* spaces. :)
     cat >&2 <<-"EOWARN"
 ****************************************************

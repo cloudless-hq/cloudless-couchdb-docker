cdb818ed857271b6ca6704c39beae6323a2ca3b0
till <till@php.net>
2018-06-28 16:30:22 +0200

    Refactor: less typing, more variables

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,19 @@
 set -e
 
 if [ "$1" = 'clouseau' ]; then
+    config="${CLOUSEAU_PATH}/etc/clouseau.ini"
+
+    if [ -z "$NODE_NAME" ]; then
+        node=$(hostname -f)
+    else
+        node=$NODE_NAME
+    fi
+
     # setup configuration
-    echo '[clouseau]' > "${CLOUSEAU_PATH}/etc/clouseau.ini"
-    echo "name=clouseau@${NODE_NAME}" >> "${CLOUSEAU_PATH}/etc/clouseau.ini"
-    echo "cookie=${ERLANG_COOKIE}" >> "${CLOUSEAU_PATH}/etc/clouseau.ini"
-    echo "dir=${INDEX_DIR}" >> "${CLOUSEAU_PATH}/etc/clouseau.ini"
+    echo '[clouseau]' > $config
+    echo "name=clouseau@${node}" >> $config
+    echo "cookie=${ERLANG_COOKIE}" >> $config
+    echo "dir=${INDEX_DIR}" >> $config
 
     # explicitely start local epmd since for scalang/clouseau there's no automatic launch mechanism like there is for erlang
     epmd -daemon
@@ -14,7 +22,7 @@ if [ "$1" = 'clouseau' ]; then
     java -Dlog4j.configuration=file:$CLOUSEAU_PATH/etc/log4j.properties \
     -cp $CLOUSEAU_PATH/etc:$CLOUSEAU_PATH/lib/* \
     com.cloudant.clouseau.Main \
-    "${CLOUSEAU_PATH}/etc/clouseau.ini"
+    $config
 fi
 
 # execute anything :-)
